971c251b522b22cd753880a5ab8d04b22a91f233
aguiarandressa <aguiar.andressa@gmail.com>
2026-02-10 17:29:12 -0300

    Update admin-donas.js

--- js/admin-donas.js
+++ js/admin-donas.js
@@ -11,7 +11,7 @@ const d_nome = document.getElementById("d_nome");
 const d_whats = document.getElementById("d_whats");
 const btnAddDona = document.getElementById("btnAddDona");
 const listaDonas = document.getElementById("listaDonas");
-const supabase = window.supabase;
+//const supabase = window.supabase;
 // =========================
 // ESTADO
 // =========================
