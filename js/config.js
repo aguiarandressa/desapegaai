884296c59a1789099b083f37181a545b426679f4
aguiarandressa <aguiar.andressa@gmail.com>
2026-02-10 04:14:16 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,14 +1,17 @@
 // =============================
-// CONFIG SUPABASE
+// CONFIGURAÇÃO SUPABASE
 // =============================
 const SUPABASE_URL = "https://ssspgiiftrhvdjidndzi.supabase.co";
 const SUPABASE_ANON_KEY = "sb_publishable_igD1PWk91A-ySowhPqJdA_484zncZX";
 const BUCKET = "produtos";
 
-// client único (evita erro de redeclaração)
+// Cria o client UMA ÚNICA VEZ
 if (!window.supabaseClient) {
   window.supabaseClient = supabaseJs.createClient(
     SUPABASE_URL,
     SUPABASE_ANON_KEY
   );
 }
+
+// Atalho opcional (leitura mais limpa)
+const supabase = window.supabaseClient;
